eb0839ebbea56d2ef2f9fae431880bfc5484841a
Hui <jeffrey.x.hui@gmail.com>
2020-05-11 17:47:55 -0700

    tests passing, but function does not perform as intended

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    print('starting_node', starting_node)
+    print('ancestors', ancestors)
+    ancestorCache = {}
+    for i in ancestors:
+        if i[1] not in ancestorCache:
+            ancestorCache[i[1]] = [i[0]]
+        else:
+            ancestorCache[i[1]].append(i[0])
+    
+    if starting_node not in ancestorCache:
+        return -1
+    
+    searcher = starting_node
+
+    while searcher in ancestorCache:
+        searcher = ancestorCache[searcher][0]
+    return searcher
+    parents = ancestorCache[starting_node]
+    print(ancestorCache[parents[0]])
