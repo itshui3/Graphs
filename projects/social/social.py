fe59185863825c648ea9524a938b475f17513e51
Hui <jeffrey.x.hui@gmail.com>
2020-05-13 14:07:13 -0700

    should be close to if not MVP

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from util import Queue
 
 class User:
     def __init__(self, name):
@@ -70,10 +71,6 @@ class SocialGraph:
         for cT in connectThese:
             self.add_friendship(cT[0], cT[1])
         
-        print('users', self.users)
-        print('friendships', self.friendships)
-        
-
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -83,14 +80,48 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
+        visitedResult = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
-        return visited
+
+        # Get Neighbors
+        neighbors = self.friendships[user_id]
+
+        q = Queue()
+        visitSet = set()
+        visitSet.add(user_id)
+
+        q.enqueue((user_id, 0))
+        visitedResult[user_id] = [user_id]
+
+        while q.size() > 0:
+            pointFriend, separation = q.dequeue()
+
+            neighbors = self.friendships[pointFriend]
+
+            if len(neighbors) > 0:
+                for n in neighbors:
+                    if n not in visitSet:
+                        q.enqueue((n, separation + 1))
+                        visitSet.add(n)
+                        visitedResult[n] = visitedResult[pointFriend] + [n]
+        return visitedResult
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph(1000, 5)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
+
+    lengthTotal = 0
+    for i, c in connections.items():
+        lengthTotal += len(c)
+
+    print(lengthTotal // 1000)
+
+    # to create 100 users with avg of 10 friends each, how many times would you need to call add_friendship? 
+    # Answer:***: 500 times, because each call adds 2 friendships
+
+    # 1k users with 5 avg friend, what % of other users will be in a particular user's extended social network? what is avg degree of separation between a user and those in their extended network? 
+
